682fb24ff02c90b02f6b85de297cbd61a197f789
Dave Kerr <dwmkerr@gmail.com>
2015-09-07 08:40:04 +0700

    Tidied up the home screen

--- src/home/home.js
+++ src/home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Home extends React.Component {
   render () {
     return <div>
-        <h1>React Redux Starter</h1>
+        <h1>React ES6 Starter</h1>
         <p>Welcome to the React ES6 Starter home page!</p>
       </div>;
   }
