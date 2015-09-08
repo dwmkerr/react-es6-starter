f3c15249d3624f2253494526fc7501e121c8689e
Dave Kerr <dwmkerr@gmail.com>
2015-09-08 08:48:41 +0700

    Fixed the website url

--- src/home/home.js
+++ src/home/home.js
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
         <p>Welcome to the React ES6 Starter home page!</p>
         <p>This is the companion application to the <a href="http://www.dwmkerr.com/getting-started-with-react/">
         Getting Started with React & ES6</a> article, you can find the code at 
-        <a href="https://github.com/dwmkerr/react-es6-starter</a>">github.com/dwmkerr/react-es6-starter</a>.</p>
+        <a href="https://github.com/dwmkerr/react-es6-starter">github.com/dwmkerr/react-es6-starter</a>.</p>
       </div>;
   }
 }
