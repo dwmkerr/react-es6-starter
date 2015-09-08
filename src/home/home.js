71d007cc80b02685350c000c8e52d7f835220462
Dave Kerr <dwmkerr@gmail.com>
2015-09-08 08:40:29 +0700

    Update home.js

--- src/home/home.js
+++ src/home/home.js
@@ -5,6 +5,9 @@ export default class Home extends React.Component {
     return <div>
         <h1>React ES6 Starter</h1>
         <p>Welcome to the React ES6 Starter home page!</p>
+        <p>This is the companion application to the <a href="http://www.dwmkerr.com/getting-started-with-react/">
+        Getting Started with React & ES6</a> article, you can find the code at 
+        <a href="https://github.com/dwmkerr/react-es6-starter</a>">github.com/dwmkerr/react-es6-starter</a>.</p>
       </div>;
   }
 }
